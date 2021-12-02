47870b00d604c53a22f7e6828c5c222759ea5af1
HelloKittycoder <shucheng2015@outlook.com>
2021-12-02 22:13:28 +0800

    17 JUC-集合线程安全-HashSet和HashMap线程不安全

--- juc-atguigu/src/main/java/com/atguigu/lock/ThreadDemo4.java
+++ juc-atguigu/src/main/java/com/atguigu/lock/ThreadDemo4.java
@@ -1,7 +1,9 @@
 package com.atguigu.lock;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * Created by shucheng on 2021/12/1 21:53
@@ -18,13 +20,38 @@ public class ThreadDemo4 {
         // List<String> list = Collections.synchronizedList(new ArrayList<>());
 
         // CopyOnWriteArrayList解决
-        List<String> list = new CopyOnWriteArrayList<>();
+        /*List<String> list = new CopyOnWriteArrayList<>();
 
         for (int i = 0; i < 10; i++) {
             new Thread(()->{
                 // 向集合添加内容
                 list.add(UUID.randomUUID().toString().substring(0,8));
                 System.out.println(Thread.currentThread().getName() + "::" + list);
+            }, String.valueOf(i)).start();
+        }*/
+
+        // 演示HashSet
+        // Set<String> set = new HashSet<>();
+        /*Set<String> set = new CopyOnWriteArraySet<>();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                // 向集合添加内容
+                set.add(UUID.randomUUID().toString().substring(0,8));
+                System.out.println(Thread.currentThread().getName() + "::" + set);
+            }, String.valueOf(i)).start();
+        }*/
+
+        // 演示HashMap
+        // Map<String, String> map = new HashMap();
+        Map<String, String> map = new ConcurrentHashMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            String key = String.valueOf(i);
+            new Thread(()->{
+                // 向集合添加内容
+                map.put(key, UUID.randomUUID().toString().substring(0,8));
+                System.out.println(Thread.currentThread().getName() + "::" + map);
             }, String.valueOf(i)).start();
         }
     }
