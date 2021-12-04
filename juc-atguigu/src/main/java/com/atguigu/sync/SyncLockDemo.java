01965546763dc1230458552f2c6031c88eeb77ab
HelloKittycoder <shucheng2015@outlook.com>
2021-12-04 12:08:33 +0800

    21 JUC-多线程锁-可重入锁ReentrantLock

--- juc-atguigu/src/main/java/com/atguigu/sync/SyncLockDemo.java
+++ juc-atguigu/src/main/java/com/atguigu/sync/SyncLockDemo.java
@@ -1,5 +1,8 @@
 package com.atguigu.sync;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by shucheng on 2021/12/4 10:19
  */
@@ -23,8 +26,37 @@ public class SyncLockDemo {
     }*/
 
     public static void main(String[] args) {
+        // Lock演示可重入锁（注意：lock和unlock要成对出现）
+        Lock lock = new ReentrantLock();
+        new Thread(() -> {
+            // 上锁
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + " 外层");
+                lock.lock();
+                try {
+                    System.out.println(Thread.currentThread().getName() + " 内层");
+                } finally {
+                    lock.unlock();
+                }
+            } finally {
+                // 解锁
+                lock.unlock();
+            }
+        }, "t1").start();
+
+        // 创建新线程
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + " aaaa");
+            } finally {
+                lock.unlock();
+            }
+        }, "aa").start();
+
         // synchronized
-        Object o = new Object();
+        /*Object o = new Object();
         new Thread(() -> {
             synchronized (o) {
                 System.out.println(Thread.currentThread().getName() + " 外层");
@@ -39,7 +71,7 @@ public class SyncLockDemo {
             }
         }, "t1").start();
 
-        new SyncLockDemo().add();
+        new SyncLockDemo().add();*/
         // new SyncLockDemo().add(0);
     }
 }
